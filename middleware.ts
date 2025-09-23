85b1d3a6344728cb78ea052a15d0188ae0759fed
Eyuel Belete <eyuelbelete98@gmail.com>
2025-09-23 23:19:51 +0300

    fixx

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,20 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { matchesUA } from "browserslist-useragent";
-import pkg from "./package.json";
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { matchesUA } from 'browserslist-useragent';
+import { supportedBrowsers } from './supportedBrowsers';
 
-export default function middleware(req: NextRequest) {
-  const ua = req.headers.get("user-agent") || "";
+export function middleware(req: NextRequest) {
+  const ua = req.headers.get('user-agent') || '';
   const isSupported = matchesUA(ua, {
-    browsers: pkg.browserslist,
+    browsers: supportedBrowsers,
     allowHigherVersions: true,
     ignoreMinor: true,
     ignorePatch: true,
   });
-  console.log({
-    isSupported,
-    ua
-  });
-  if (!isSupported) {
-    return NextResponse.redirect(new URL("/v2", req.url));
-  }
 
+  if (!isSupported) {
+    return NextResponse.redirect(new URL('/v2', req.url));
+  }
   return NextResponse.next();
 }
---
+++ supportedBrowsers.ts
@@ -0,0 +1,6 @@
+// supportedBrowsers.ts
+export const supportedBrowsers = [
+  "last 2 versions",
+  "not dead",
+  "not ie <= 11",
+];
